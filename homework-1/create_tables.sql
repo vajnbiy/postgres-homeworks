ec15cb2a011332f97d8aadc33412e191f09283e1
Evgeny <vajnbiy@gmail.com>
2023-07-14 11:00:55 -0700

    dz

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,24 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees (
+	emp_id int PRIMARY KEY,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	title varchar(50),
+	birth_date date NOT NULL,
+	notes text
+);
+
+CREATE TABLE customers (
+	customer_id char(5) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(50)
+);
+
+CREATE TABLE order_data (
+	order_id varchar(10) PRIMARY KEY,
+	customer_id char(5) REFERENCES customers(customer_id),
+	emp_id int REFERENCES employees(emp_id),
+	order_date date NOT NULL,
+	ship_city varchar(50)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,38 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import csv
+import psycopg2
+
+with open('north_data/employees_data.csv', newline='\n') as file:
+    emp_data = list(csv.reader(file, delimiter=',', quotechar='"'))
+
+with open('north_data/customers_data.csv', newline='\n') as file:
+    customer_data = list(csv.reader(file, delimiter=',', quotechar='"'))
+
+with open('north_data/orders_data.csv', newline='\n') as file:
+    orders_data = list(csv.reader(file, delimiter=',', quotechar='"'))
+
+
+a = input('Password\n')
+
+with psycopg2.connect(
+    database='north',
+    user='postgres',
+    password=a,
+    host='localhost') as conn:
+    with conn.cursor() as cur:
+        for entry in emp_data[1:]:
+            cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)',
+                        (entry[0], entry[1], entry[2], entry[3], entry[4], entry[5]))
+
+    with conn.cursor() as cur:
+        for entry in customer_data[1:]:
+            cur.execute('INSERT INTO customers VALUES (%s, %s, %s)',
+                        (entry[0], entry[1], entry[2]))
+
+    with conn.cursor() as cur:
+        for entry in orders_data[1:]:
+            cur.execute('INSERT INTO order_data VALUES (%s, %s, %s, %s, %s)',
+                        (entry[0], entry[1], entry[2], entry[3], entry[4]))
+
+conn.close()
